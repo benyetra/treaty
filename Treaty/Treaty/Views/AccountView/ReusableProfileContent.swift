65f448b97fb0d6e27afab5967aeb38ae4c560bbe
Bennett Yetra <bennett.yetra@hulu.com>
2023-01-18 12:17:16 -0500

    basic account and log out work

---
+++ Treaty/Treaty/Views/AccountView/ReusableProfileContent.swift
@@ -0,0 +1,47 @@
+//
+//  ReusableProfileContent.swift
+//  Treaty
+//
+//  Created by Bennett Yetra on 1/18/23.
+//
+
+import SwiftUI
+import SDWebImageSwiftUI
+
+struct ReusableProfileContent: View {
+    var user: User
+    
+    var body: some View {
+        ScrollView(.vertical, showsIndicators: false) {
+            LazyVStack{
+                HStack(spacing: 12){
+                    WebImage(url: user.userProfileURL).placeholder{
+                        // MARK: Placeholder Imgae
+                        Image("NullProfile")
+                            .resizable()
+                    }
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 100, height: 100)
+                    .clipShape(Circle())
+                    
+                    VStack(alignment: .leading, spacing: 6) {
+                        Text(user.username)
+                            .font(.title3)
+                            .fontWeight(.semibold)
+                    }
+                    .hAlign(.leading)
+                }
+                
+                Text("Post's")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.black)
+                    .hAlign(.leading)
+                    .padding(.vertical,15)
+                
+            }
+            .padding(15)
+        }
+    }
+}
