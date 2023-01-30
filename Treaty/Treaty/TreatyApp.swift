c8cf489638b31e8988276049bae6e3b56920ba47
Bennett Yetra <bennett.yetra@hulu.com>
2023-01-29 20:45:49 -0500

    asking correctly for push access

--- Treaty/Treaty/TreatyApp.swift
+++ Treaty/Treaty/TreatyApp.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import Firebase
+import FirebaseMessaging
 import GoogleSignIn
 
 @main
@@ -19,19 +20,119 @@ struct TreatyApp: App {
     }
 }
 
-// MARK: Initializing Firebase
+// Configuring Firebase Push Notifications...
+// See my Full Push Notification Video..
+// Link in Description...
+
+// Intializng Firebase And CLoud Messaging...
+
 class AppDelegate: NSObject,UIApplicationDelegate{
+    
+    let gcmMessageIDKey = "gcm.message_id"
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        
         FirebaseApp.configure()
-        if let clientID = FirebaseApp.app()?.options.clientID{
-            let config = GIDConfiguration(clientID: clientID)
-            GIDSignIn.sharedInstance.configuration = config
+        
+        // Setting Up Cloud Messaging...
+        
+        Messaging.messaging().delegate = self
+        
+        // Setting Up Notifications...
+        
+        if #available(iOS 10.0, *) {
+          // For iOS 10 display notification (sent via APNS)
+          UNUserNotificationCenter.current().delegate = self
+
+          let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+          UNUserNotificationCenter.current().requestAuthorization(
+            options: authOptions,
+            completionHandler: {_, _ in })
+        } else {
+          let settings: UIUserNotificationSettings =
+          UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+          application.registerUserNotificationSettings(settings)
         }
+
+        application.registerForRemoteNotifications()
         return true
     }
     
-    // MARK: Phone Auth Needs to Intialize Remote Notifications
-    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any]) async -> UIBackgroundFetchResult {
-        return .noData
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+                     fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+
+        // DO Something With Message Data Here....
+      if let messageID = userInfo[gcmMessageIDKey] {
+        print("Message ID: \(messageID)")
+      }
+
+      // Print full message.
+      print(userInfo)
+
+      completionHandler(UIBackgroundFetchResult.newData)
+    }
+    
+    // In order to receive notifications you need implement thsese methods...
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        
+    }
+
+}
+
+// CLoud Messaging...
+extension AppDelegate: MessagingDelegate{
+    
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+    
+        // Store this token to firebase and retrieve when to send message to someone....
+        let dataDict:[String: String] = ["token": fcmToken ?? ""]
+        
+        // Store token in Firestore For Sending Notifications From Server in Future...
+        
+        print(dataDict)
     }
 }
+
+// User Notifications...[AKA InApp Notifications...]
+
+@available(iOS 10, *)
+extension AppDelegate : UNUserNotificationCenterDelegate {
+
+  func userNotificationCenter(_ center: UNUserNotificationCenter,
+                              willPresent notification: UNNotification,
+    withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+    let userInfo = notification.request.content.userInfo
+
+    // DO Something With MSG Data...
+    
+    if let messageID = userInfo[gcmMessageIDKey] {
+      print("Message ID: \(messageID)")
+    }
+
+    print(userInfo)
+
+    completionHandler([[.banner,.badge, .sound]])
+  }
+
+  func userNotificationCenter(_ center: UNUserNotificationCenter,
+                              didReceive response: UNNotificationResponse,
+                              withCompletionHandler completionHandler: @escaping () -> Void) {
+    let userInfo = response.notification.request.content.userInfo
+    if let messageID = userInfo[gcmMessageIDKey] {
+      print("Message ID: \(messageID)")
+    }
+
+    // DO Something With MSG Data...
+    print(userInfo)
+
+    completionHandler()
+  }
+}
+
+
