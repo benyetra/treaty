bedbae4fed06b5f83a8d38319e48697e2e6f5be1
Bennett Yetra <bennett.yetra@hulu.com>
2023-01-30 15:38:21 -0500

    display partner username in profile

--- Treaty/Treaty/Views/Account View/ReusableProfileContent.swift
+++ Treaty/Treaty/Views/Account View/ReusableProfileContent.swift
@@ -7,10 +7,16 @@
 
 import SwiftUI
 import SDWebImageSwiftUI
+import FirebaseFirestoreSwift
+import Firebase
+import FirebaseStorage
+import FirebaseFirestore
 
 struct ReusableProfileContent: View {
     var user: User
-    
+    @State private var partnerUsername: String = ""
+    @State private var partnerToken: String = ""
+
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
             LazyVStack{
@@ -29,6 +35,14 @@ struct ReusableProfileContent: View {
                         Text("@\(user.username)")
                             .font(.title3)
                             .fontWeight(.semibold)
+                        if partnerUsername != "" {
+                            Text("Partner: @\(partnerUsername)")
+                        } else {
+                            Text("Add your partner in the account menu")
+                        }
+                    }
+                    .onAppear {
+                        getPartnerData()
                     }
                     .hAlign(.leading)
                 }
@@ -44,4 +58,32 @@ struct ReusableProfileContent: View {
             .padding(15)
         }
     }
+    
+    func getPartnerData() {
+        let db = Firestore.firestore()
+        guard let uid = Auth.auth().currentUser?.uid else {
+            print("nil")
+            return
+        }
+        db.collection("Users").document(uid).getDocument { (document, error) in
+            if let document = document, document.exists {
+                let partnerUID = document["partners"] as? String ?? ""
+                if partnerUID.isEmpty {
+                    print("Partner UID not found in user document")
+                    return
+                }
+                db.collection("Users").document(partnerUID).getDocument { (partnerDocument, error) in
+                    if let partnerDocument = partnerDocument, partnerDocument.exists {
+                        self.partnerUsername = partnerDocument["username"] as? String ?? ""
+                        self.partnerToken = partnerDocument["token"] as? String ?? ""
+                    } else {
+                        print("Error getting partner data: \(error)")
+                    }
+                }
+            } else {
+                print("Error getting user data: \(error)")
+            }
+        }
+    }
 }
+
